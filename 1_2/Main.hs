d0cb57da5701930c0d5382e9a145d208eaca74e6
Hugo Sandelius <hugo.sandelius@gmail.com>
2019-12-01 15:16:32 +0100

    Cleanup

--- 1_2/Main.hs
+++ 1_2/Main.hs
@@ -3,4 +3,4 @@ main = print . sum . map (fuel . read) . lines =<< readFile "input.txt"
 
 fuel :: Integer -> Integer
 fuel mass = if f <= 0 then 0 else f + fuel f 
-    where f = (flip (-) 2 . flip div 3) mass
+    where f = mass `div` 3 - 2
