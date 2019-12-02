18cb0e3c938e773b8d8647a8c43a7ad690880e61
Hugo Sandelius <hugo.sandelius@gmail.com>
2019-12-02 23:01:38 +0100

    Add day 2, part 1

---
+++ 2_1/Main.hs
@@ -0,0 +1,22 @@
+import Data.Sequence
+import Data.Text(pack, unpack, splitOn)
+
+main :: IO ()
+main = print . flip index 0 . interpret 0 . restore . fromList . map read . split "," =<< readFile "input.txt"
+
+restore :: Seq Int -> Seq Int
+restore = update 1 12 . update 2 2
+
+interpret :: Int -> Seq Int -> Seq Int
+interpret i program =
+    case index program i of
+        1 -> instruct (+)
+        2 -> instruct (*)
+        99 -> program
+    where 
+        instruct op = interpret (i + 4) $ update (off 3) ((ix . off $ 1) `op` (ix . off $ 2)) program
+        ix = index program
+        off n = ix $ i + n
+
+split :: String -> String -> [String]
+split s = map unpack . splitOn (pack s) . pack
